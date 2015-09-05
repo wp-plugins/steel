8f221cf6c7b0a93120c3c204b71e596ed9f80655
starverte <starverte@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-05 00:12:39 +0000

    Tag Steel 1.2.7
    git-svn-id: https://plugins.svn.wordpress.org/steel/tags/1.2.7@1238426 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,124 @@
+=== Steel ===
+Contributors: starverte, mbeall
+Tags: bootstrap, carousel, quotes, shortcodes, slides, teams, widgets
+Requires at least: 3.9
+Tested up to: 4.3
+Stable tag: 1.2.7
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl.html
+
+Core plugin of the Sparks Framework. Works for any theme; but when paired with Flint your WordPress site will be on fire.
+
+== Description ==
+
+Steel adds certain capabilities through several optional modules. We are continuing to add modules, so stay tuned.
+
+= Bootstrap =
+Bootstrap 3 is included with Steel, and comes alive through newly added shortcodes!
+
+= Quotes =
+Display a random quote (or testimonial) in the sidebar with our random quote widget. We are looking at embedding random quotes within a post or page in our next update.
+
+= Slides =
+Create and display media slideshows using Bootstrap’s Carousel plugin.
+
+= Teams =
+Create profiles for board of directors, staff members, etc. and display them on a nice summary page.
+
+= Future Modules =
+* Aliases - Have multiple "pretty permalinks" point to the same post or page, or create a "pretty permalink" for an offsite link.
+* Events - Add an events calendar to your WordPress site
+* Podcast - Add a podcast with different channels and different feeds, available to listen online or via iTunes
+* Styles - This module will replace the need for shortcodes by adding buttons and styles to TinyMCE.
+
+Suggestions for other modules? Let us know via the Support tab.
+
+Like our plugin? Write us a review!
+
+== Installation ==
+
+1. Upload `steel.zip` to the `/wp-content/plugins/` directory
+1. Unzip `steel.zip` to a sub-directory named `steel`
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Check out the options menu for more options
+
+== Frequently Asked Questions ==
+
+Submit your questions to http://sparks.starverte.com/steel and we will try to answer them.
+
+== Changelog ==
+= 1.2.7 =
+- Changed `$this->WP_Widget` to `$this->__construct` (deprecated with WordPress 4.3)
+
+= 1.2.6 =
+- Upgraded to Bootstrap 3.3.5
+- Code quality improvements
+
+= 1.2.5 =
+- Code quality improvements
+- Bug fix: Gallery skin outputs links as expected
+
+= 1.2.4 =
+- Update core colors for WordPress 4.2
+- New Gallery skin for Slides
+- New functions `steel_get_slides` and `steel_sanitize_get_slides` for use in building `<select>` fields
+
+= 1.2.3 =
+* Fixed fatal error in slides.php
+
+= 1.2.2 =
+* Added target attribute to btn shortcode
+* Various UI improvements
+* Added Google Analytics module
+* Upgrade to Bootstrap 3.3.4 and point to MaxCDN's bootstrap files
+
+= 1.2.0 =
+* Options UI improvements
+* Added grid.css
+* Further development of Podcast module (still inactive)
+* Upgrade to Bootstrap 3.3.2
+
+= 1.1.7 =
+* Clean up code
+* Upgrade to Bootstrap 3.2
+
+For more details, check out https://github.com/starverte/steel/pull/126
+
+= 1.1.6 =
+* Fixed `run.js` to avoid conflict with new Audio List feature in WordPress 3.9
+* Fixed columns shortcode to reflect Bootstrap integration
+* Added transitions to Slides module
+* New Navigation Menu widget
+
+For more details, check out https://github.com/starverte/steel/pull/125
+
+= 1.1.4 =
+* Simplified editor in Slides
+* Removed templates and integrated new Flint action hooks
+* Upgraded to Bootstrap 3.1.1
+
+= 1.1.3 =
+* Caption in "Bar" skin now overlays image instead of appearing beneath the image
+* jQuery conflicts fixed
+
+= 1.1.2 =
+Extracted carousel and glyphicon styles from bootstrap.css so that carousel works even if Bootstrap module isn't active
+
+= 1.1.1 =
+* Added skins for Slides module
+* Added ability to turn off Bootstrap module
+
+= 1.1.0 =
+* Added numerous shortcodes to implement Bootstrap like [btn] and [glyph] (for complete list and use, check out http://sparks.starverte.com/steel/bootstrap/)
+* Added new Slides module (documentation will soon be available via http://sparks.starverte.com/steel/slides/)
+
+For more details, check out https://github.com/starverte/steel/pull/102
+
+= 1.0.1 =
+Removed redundant `steel_version` and admin scripts functions
+
+= 1.0.0 =
+Updated branding and descriptions.
+
+= 0.8.2 =
+Initial release
---
+++ steel.php
@@ -0,0 +1,329 @@
+<?php
+/*
+Plugin Name: Steel
+Plugin URI: https://github.com/starverte/steel.git
+Description: Core plugin of the Sparks Framework. Works for any theme; but when paired with Flint your WordPress site will be on fire.
+Version: 1.2.7
+Author: Star Verte LLC
+Author URI: http://starverte.com/
+License: GPLv3
+License URI: http://www.gnu.org/licenses/
+
+  Copyright 2013-2015 Star Verte LLC (email : dev@starverte.com)
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+include_once dirname( __FILE__ ) . '/bootstrap.php';
+include_once dirname( __FILE__ ) . '/options.php';
+
+  if (is_module_active('podcast'    )) { include_once dirname( __FILE__ ) . '/podcast.php';     }
+  if (is_module_active('quotes'     )) { include_once dirname( __FILE__ ) . '/quotes.php';      }
+  if (is_module_active('shortcodes' )) { include_once dirname( __FILE__ ) . '/shortcodes.php';  }
+  if (is_module_active('slides'     )) { include_once dirname( __FILE__ ) . '/slides.php';      }
+  if (is_module_active('teams'      )) { include_once dirname( __FILE__ ) . '/teams.php';       }
+  if (is_module_active('widgets'    )) { include_once dirname( __FILE__ ) . '/widgets.php';     }
+
+if (is_flint_active()) { include_once dirname( __FILE__ ) . '/templates.php'; }
+
+/**
+ * Returns current plugin version.
+ *
+ * Deprecated Steel 1.2. Use actual version number instead.
+ * @TODO Remove backwards compatibility in Steel 1.4
+ */
+function steel_version() { return '1.2.1'; }
+
+/**
+ * Load scripts
+ */
+add_action( 'admin_enqueue_scripts', 'steel_admin_scripts' );
+function steel_admin_scripts() {
+  wp_enqueue_style( 'dashicons'                                                  );
+  wp_enqueue_style( 'bs-glyphicons'    , plugins_url('steel/css/glyphicons.css') );
+  wp_enqueue_style( 'bs-grid'          , plugins_url('steel/css/grid.css'      ) );
+  wp_enqueue_style( 'steel-admin-style', plugins_url('steel/css/admin.css'     ) );
+  wp_enqueue_style( 'steel-font'       , plugins_url('steel/css/starverte.css' ) );
+
+  wp_enqueue_script( 'jquery'              );
+  wp_enqueue_script( 'jquery-ui-core'      );
+  wp_enqueue_script( 'jquery-ui-accordion');
+  wp_enqueue_script( 'jquery-ui-datepicker');
+  wp_enqueue_script( 'jquery-ui-sortable'  );
+  wp_enqueue_script( 'jquery-ui-position'  );
+  wp_enqueue_script( 'jquery-effects-core' );
+  wp_enqueue_script( 'jquery-effects-blind');
+
+  wp_enqueue_media();
+
+  if (is_module_active('podcast')) {
+    wp_enqueue_script( 'podcast-mod', plugins_url('steel/js/podcast.js'  ), array('jquery'), '1.2.1', true );
+    wp_enqueue_script( 'podcast-channel', plugins_url('steel/js/podcast-channel.js'  ), array('jquery'), '1.2.1', true );
+  }
+
+  if (is_module_active('slides')) {
+    wp_enqueue_script( 'slides-mod', plugins_url('steel/js/slides.js'  ), array('jquery'), '1.2.1', true );
+  }
+}
+add_action( 'wp_enqueue_scripts', 'steel_scripts' );
+function steel_scripts() {
+  $options = steel_get_options();
+
+  if (true === $options['load_bootstrap_js']) {
+    // Make sure there aren't other instances of Twitter Bootstrap
+    wp_deregister_script('bootstrap');
+
+    // Load Twitter Bootstrap
+    wp_enqueue_script( 'bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js', array('jquery'), '3.3.5', true );
+  }
+
+  if (true === $options['load_bootstrap_css']) {
+    // Make sure there aren't other instances of Twitter Bootstrap
+    wp_deregister_style('bootstrap-css');
+
+    // Load Twitter Bootstrap
+    wp_enqueue_style( 'bootstrap-css', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css', array() , '3.3.5' );
+  }
+  else {
+    wp_enqueue_style( 'glyphicons', plugins_url('steel/css/glyphicons.css'), array() , '3.3.5' );
+  }
+
+  if (is_module_active('slides')) {
+    wp_enqueue_style( 'slides-mod-style', plugins_url('steel/css/slides.css'  ), array(), '1.2.1');
+  }
+
+  // Load script for "Pin It" button
+  wp_enqueue_script( 'pin-it-button', 'http://assets.pinterest.com/js/pinit.js');
+
+  // Load front-end scripts
+  wp_enqueue_script( 'steel-run', plugins_url( '/steel/js/run.js' ), array('jquery'), '1.2.1', true );
+}
+
+/*
+ * Add function steel_open
+ */
+add_action('flint_open','steel_open');
+function steel_open() {
+  $options = steel_get_options();
+
+  if (true === $options['load_facebook']  && !empty($options['fb_app_id'])) {
+      echo '<div id="fb-root"></div><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=' . $options['fb_app_id'] . '"; fjs.parentNode.insertBefore(js, fjs); }(document, \'script\', \'facebook-jssdk\')); </script>';
+  }
+  else { return; }
+}
+
+/*
+ * Empty search fix
+ */
+add_filter( 'request', 'steel_request' );
+function steel_request( $query_vars ) {
+  if( !empty( $_GET['s'] ) && empty( $_GET['s'] ) ) { $query_vars['s'] = " "; }
+    return $query_vars;
+}
+
+/*
+ * Create function tweet_this
+ */
+function tweet_this( $data_count = 'horizontal' , $data_size = '' , $data_via = '' , $args = array() ) {
+  $url      = get_permalink();
+  $title    = the_title( '', '', false);
+  $language = get_bloginfo( 'language' );
+
+  $defaults = array(
+    'data_url'      => $url,
+    'data_text'     => $title,
+    'data_related'  => '',
+    'data_lang'     => $language,
+    'data_counturl' => $url,
+    'data_hashtags' => '',
+    'data_dnt'      => '',
+  );
+
+  $args = wp_parse_args($args, $defaults);
+  $args = (object) $args;
+
+  if ( '' !== $args->data_hashtags ) {
+    $tweet_class = 'twitter-hashtag-button';
+    $hashtag     = '#' . $args->data_hashtags;
+    $link        = 'https://twitter.com/intent/tweet?button_hashtag=' . $hashtag;
+  }
+  else {
+    $tweet_class = 'twitter-share-button';
+    $hashtag     = '';
+    $link        = 'https://twitter.com/share';
+  }
+
+  printf(
+    '<a href="%s" class="%s" data-count="%s" data-size="%s" data-via="%s" data-url="%s" data-text="%s" data-related="%s" data-lang="%s" data-counturl="%s" data_hashtags="%s" data-dnt="%s">Tweet</a>',
+
+    $link,
+    $tweet_class,
+    $data_count,
+    $data_size,
+    $data_via,
+    $args->data_url,
+    $args->data_text,
+    $args->data_related,
+    $args->data_lang,
+    $args->data_counturl,
+    $args->data_hashtags,
+    $args->data_dnt
+  );
+  printf('<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>');
+}
+
+/*
+ * Create function like_this (Facebook)
+ */
+function like_this( $args = array() ) {
+  $url = get_permalink();
+
+  $defaults = array(
+    'data_href'       => $url,
+    'data_send'       => 'false',
+    'data_layout'     => 'standard',
+    'data_show_faces' => 'false',
+    'data_width'      => '450',
+    'data_action'     => 'like',
+    'data_font'       => 'lucida grande',
+    'data_color'      => 'light',
+    'data_ref'        => '',
+  );
+
+  $args = wp_parse_args($args, $defaults);
+  $args = (object) $args;
+
+  printf(
+    '<div class="fb-like" data-href="%s" data-send="%s" data-layout="%s" data-show-faces="%s" data-width="%s" data-action="%s" data-font="%s" data-colorscheme="%s" data-ref="%s"></div>',
+
+    $args->data_href,
+    $args->data_send,
+    $args->data_layout,
+    $args->data_show_faces,
+    $args->data_width,
+    $args->data_action,
+    $args->data_font,
+    $args->data_color,
+    $args->data_ref
+  );
+}
+
+/*
+ * Create function pin_it (Pinterest)
+ */
+function pin_it( $args = array() ) {
+  $url       = get_permalink();
+  $title     = the_title( '', '', false);
+  $thumb_id  = get_post_thumbnail_id();
+  $thumbnail = wp_get_attachment_url( $thumb_id );
+
+  $defaults = array(
+    'data_url' => $url,
+    'data_thumb' => $thumbnail,
+    'data_text' => $title,
+    'data_count' => 'horizontal',
+  );
+
+  $args = wp_parse_args($args, $defaults);
+  $args = (object) $args;
+
+  printf(
+    '<a href="http://pinterest.com/pin/create/button/?url=%s&media=%s&description=%s" class="pin-it-button" count-layout="%s"><img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" /></a>',
+
+    $args->data_url,
+    $args->data_thumb,
+    $args->data_text,
+    $args->data_count
+  );
+}
+
+/*
+ * Add function is_module_active
+ */
+function is_module_active( $module ) {
+  $options = steel_get_options();
+
+  if (true === $options['load_'.$module])
+    return true;
+  else
+    return false;
+}
+
+/*
+ * Create function to remove admin bar from front end by default
+ */
+add_action('after_setup_theme', 'remove_admin_bar');
+function remove_admin_bar() {
+  if (!current_user_can('administrator') && !is_admin()) {
+    show_admin_bar(false);
+  }
+}
+
+/*
+ * Add function is_flint_active
+ */
+function is_flint_active() {
+  $theme = wp_get_theme();
+  $name = $theme->get( 'Name' );
+  $template = $theme->get( 'Template' );
+  $template = !empty($template) ? $template : strtolower ( $name );
+  if ('flint' === $template)
+    return true;
+  else
+    return false;
+}
+
+/*
+ * Add function steel_get_image_url
+ */
+function steel_get_image_url( $attachment_id, $size = 'thumbnail', $icon = false ) {
+  $image = wp_get_attachment_image_src( $attachment_id, $size, $icon );
+  return $image[0];
+}
+
+/*
+ * Display custom metadata
+ */
+function steel_meta( $mod_prefix, $key, $post_id = NULL ) {
+  global $post;
+  $custom = NULL === $post_id ? get_post_custom($post->ID) : get_post_custom($post_id);
+  $meta = !empty($custom[$mod_prefix.'_'.$key][0]) ? $custom[$mod_prefix.'_'.$key][0] : '';
+  return $meta;
+}
+
+add_action('wp_footer','steel_ga_script');
+function steel_ga_script() {
+  $options = steel_get_options();
+
+  $ga_id = $options['ga_id'];
+
+  if (!empty($ga_id)) {
+    if (is_user_logged_in()) { ?>
+      <!-- Google Analytics code disabled because user is logged in. -->
+      <?php
+    }
+    else { ?>
+      <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', '<?php echo $ga_id; ?>']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+      </script><?php
+    }
+  }
+}
